4f37fff5586e08c47d5147888a3fe858a6b829b4
lovezeropython <haotianseo@gmail.com>
2018-01-18 10:19:57 +0800

    添加注释

--- 手写一个迭代器.py
+++ 手写一个迭代器.py
@@ -7,14 +7,17 @@
 """
 
 class Data():
-    # 第一
+    # 创建一个可迭代的类
     def __init__(self):
+        # 初始化一个变量_new_data 为列表
         self._new_data = []
 
     def add(self, i):
+        # 向 _new_data 变量中添加数据
         self._new_data.append(i)
 
     def data(self):
+         # 让_new_data 变成可迭代的对象
         return iter(self._new_data)
 
 
