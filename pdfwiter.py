257a6b7b02408eb224a79613699d20a9805e94e2
lovezeropython <haotianseo@gmail.com>
2018-01-22 14:54:48 +0800

    pdf 读取 脚本更新

---
+++ pdfwiter.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@Time: 2018/1/22
+@Author: songhao
+@微信公众号: zeropython
+@File: pdfwiter.py
+"""
+"""
+pip3 install pdfminer3k
+"""
+from urllib.request import urlopen
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from io import StringIO
+from io import open
+
+def readPDF(pdfFile):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+
+    process_pdf(rsrcmgr, device, pdfFile)
+    device.close()
+
+    content = retstr.getvalue()
+    retstr.close()
+    return content
+
+pdfFile = urlopen("http://pythonscraping.com/pages/warandpeace/chapter1.pdf")
+outputString = readPDF(pdfFile)
+print(outputString)
+pdfFile.close()
