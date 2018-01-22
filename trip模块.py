fabbfa569a8de3b5e0460fd7b9e3b32b8ad9bea2
lovezeropython <haotianseo@gmail.com>
2018-01-22 15:52:16 +0800

    删除 crawl

--- trip模块.py
+++ trip模块.py
@@ -5,12 +5,3 @@
 @微信公众号: zeropython
 @File: trip模块.py
 """
-import time, functools
-import requests,trip
-
-@trip.coroutine
-def main():
-    global r
-    r = yield trip.get('https://github.com/timeline.json')
-
-trip.run(main)
