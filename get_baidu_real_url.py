5803246dee5ad59c4751040b0e2f9584d3d03a31
lovezeropython <haotianseo@gmail.com>
2018-01-22 09:47:09 +0800

    tqdm 脚本更新

--- get_baidu_real_url.py
+++ get_baidu_real_url.py
@@ -8,18 +8,20 @@
 """
 
 
-
+# 引入库文件
 import requests
 import urllib.request
+
 bd_url = "https://www.baidu.com/link?url=mhMx_W4kSIqeHdckh0dvrBt4LDIxvTrf1XqoDQKAptW&amp;" \
          "ck=5341.10.0.0.0.203.232.0&amp;shh=www.baidu.com&amp;sht=baiduhome_pg&amp;wd=&amp;" \
          "eqid=cd90b17a00034b1c000000035a645fd5"
+# 解密方法一：
 
 r = requests.get(bd_url)
-print(r.url)
+print(r.url, "解密方法一")
 
-
+# 解密方法二：
 # python3下和Python2.7 下urlopen 引入不同
 r = urllib.request.urlopen(bd_url)
 
-print(r.geturl())
+print(r.geturl(), "解密方法二")
