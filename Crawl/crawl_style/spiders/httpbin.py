655d04707f9310e87e57d236132cbfe9accfc7e4
lovezeropython <haotianseo@gmail.com>
2018-01-22 15:01:18 +0800

    crawl scrapy 项目更新

---
+++ Crawl/crawl_style/spiders/httpbin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class HttpbinSpider(scrapy.Spider):
+    name = 'httpbin'
+    allowed_domains = ['httpbin.org']
+    start_urls = ['http://httpbin.org/']
+
+    def parse(self, response):
+        pass
