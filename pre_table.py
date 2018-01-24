0487621629dd4c42c70baf01a2dd5fde7196164e
lovezeropython <haotianseo@gmail.com>
2018-01-24 08:45:34 +0800

    更新代码 缩进问题

--- pre_table.py
+++ pre_table.py
@@ -14,10 +14,10 @@ table = PrettyTable(["key", "search", "cate"])
 with open('1.csv') as f:
     for line in f:
         row = line.split(',')
-    key = row[0]
-    search = int(row[1])
-    # 一定要转换成数字 不然排序不生效
-    cate = row[2]
-    table.add_row([key, search, cate])
+        key = row[0]
+        search = int(row[1])
+        # 一定要转换成数字 不然排序不生效
+        cate = row[2]
+        table.add_row([key, search, cate])
 
 print(table.get_string(sortby="search", reversesort=True))
