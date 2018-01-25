0a40bef34225d66aef9d189f1a9ab548d21cba48
lovezeropython <haotianseo@gmail.com>
2018-01-25 16:39:00 +0800

    windows 打包exe

---
+++ exe.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@Time: 2018/1/25
+@Author: songhao
+@微信公众号: zeropython
+@File: exe.py
+"""
+def print_hello():
+    print("hello")
+
+if __name__ == '__main__':
+    print_hello()
+"""
+                                            icon位置                                             脚本文件     
+pyinstaller --onefile --nowindowed --icon=" D:\Queena\PyCharmProjects\dist1\computer_three.ico" guess_exe.py
+
+"""
